c766b2e83d3821d2dc3c2996c06af119b08407bb
simon <silly_simon93@hotmail.com>
2023-11-26 23:06:09 +0100

    load ascon in c program and encrypt/decrypt

--- ascon_provider/ascon128/demo.c
+++ ascon_provider/ascon128/demo.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
 #include "api.h"
 #if defined(AVR_UART)
 #include "avr_uart.h"
@@ -14,6 +13,14 @@
 #include "crypto_auth.h"
 #endif
 
+#include <openssl/evp.h>
+#include <openssl/provider.h>
+#include <openssl/params.h>
+#include <openssl/core_names.h>
+#include <openssl/core.h>
+#include <openssl/provider.h>
+#include <openssl/err.h>
+
 void print(unsigned char c, unsigned char* x, unsigned long long xlen) {
   unsigned long long i;
   printf("%c[%d]=", c, (int)xlen);
@@ -22,7 +29,37 @@ void print(unsigned char c, unsigned char* x, unsigned long long xlen) {
 }
 
 int main() {
-  printf("hej\n");
+   // Initialize OpenSSL
+  /*OpenSSL_add_all_algorithms();
+
+  // Load the provider library
+  OPENSSL_init_crypto(OPENSSL_INIT_LOAD_CONFIG, NULL);
+
+  // Set the provider name (replace "your_provider_name" with the actual provider name)
+  const char* provider_name = "vigenere";
+
+  // Load the provider
+  OSSL_PROVIDER* provider = OSSL_PROVIDER_load(NULL, provider_name); */
+  const char* provider_name = "oqsprovider";
+  OSSL_LIB_CTX *ossl_ctx = OSSL_LIB_CTX_new();
+  OSSL_PROVIDER *ascon_provider = OSSL_PROVIDER_load(ossl_ctx, provider_name);
+  OSSL_PROVIDER *default_provider = OSSL_PROVIDER_load(ossl_ctx, "default");
+  int res = OSSL_PROVIDER_available(ossl_ctx, provider_name);
+  printf("Provider available: %d\n",  res);
+
+  //OpenSSL_add_all_algorithms();
+  //based on this example: https://github.com/openssl/openssl/blob/master/demos/cipher/aesgcm.c
+
+  EVP_CIPHER_CTX *encryption_ctx;
+   /* Create a context for the encrypt operation */
+  if ((encryption_ctx = EVP_CIPHER_CTX_new()) == NULL)
+      printf("error asdas\n");
+ 
+  const EVP_CIPHER *cipher = EVP_CIPHER_fetch(NULL, "vigenere", NULL);
+  if (cipher == NULL) {
+      fprintf(stderr, "cipher not available in the provider\n");
+  } 
+
   unsigned char n[CRYPTO_NPUBBYTES] = {0, 1, 2,  3,  4,  5,  6,  7,
                                        8, 9, 10, 11, 12, 13, 14, 15};
   unsigned char k[CRYPTO_KEYBYTES] = {0, 1, 2,  3,  4,  5,  6,  7,
@@ -35,12 +72,58 @@ int main() {
   unsigned long long alen = 16;
   unsigned long long clen = CRYPTO_ABYTES;
   int result = 0;
+  int outlen, tmplen;
+  unsigned char outbuf[1024];
+
+  if (!EVP_EncryptInit_ex2(encryption_ctx, cipher, k, n, NULL))
+      printf("error init\n");
+
+  /* Encrypt plaintext */
+  if (!EVP_EncryptUpdate(encryption_ctx, outbuf, &outlen, m, mlen))
+      printf("error encrypting\n");
+
+    /* Output encrypted block */
+  printf("Ciphertext:\n");
+  BIO_dump_fp(stdout, outbuf, outlen);
+
+if (!EVP_EncryptFinal_ex(encryption_ctx, outbuf, &tmplen))
+        printf("error finalize\n");
+    
+/*** DECRYPTION ***/ 
+
+printf("OPENSSL DECRYPTION: \n");
+EVP_CIPHER_CTX *decryption_ctx;
+unsigned char outbuf_decryption[1024];
+int outlen_decryption, rv;
+
+if ((decryption_ctx = EVP_CIPHER_CTX_new()) == NULL)
+    printf("error ctx init\n");
+
+/*
+  * Initialise an encrypt operation with the cipher/mode, key, IV and
+  * IV length parameter.
+  */
+if (!EVP_DecryptInit_ex2(decryption_ctx, cipher, k, n, NULL))
+    printf("decrypt init fail\n");
+
+/* Decrypt plaintext */
+if (!EVP_DecryptUpdate(decryption_ctx, outbuf_decryption, &outlen_decryption, outbuf, outlen))
+    printf("decrypt fail\n");
+
+/* Output decrypted block */
+  printf("Plaintext:\n");
+  BIO_dump_fp(stdout, outbuf_decryption, outlen_decryption);
+  
+  // ADD Tag verification check also
+
+  
 #if defined(AVR_UART)
   avr_uart_init();
   stdout = &avr_uart_output;
   stdin = &avr_uart_input_echo;
 #endif
 #if defined(CRYPTO_AEAD)
+  printf("\n\n *** native ASCON-C decrypt/encrypt starts ***\n\n");
   print('k', k, CRYPTO_KEYBYTES);
   printf(" ");
   print('n', n, CRYPTO_NPUBBYTES);
